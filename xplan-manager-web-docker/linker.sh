eb832aaddb4d48a310096cd2e6cae25461f6472f
Stephan Reichhelm <reichhelm@grit.de>
2021-03-26 10:31:05 +0100

    * update scripts

--- xplan-manager-web-docker/linker.sh
+++ xplan-manager-web-docker/linker.sh
@@ -2,7 +2,7 @@
 
 TARGET=$1
 shift
-
+echo "Start the optimization of $@"
 for DIR in $@
 do
     for FILE in $DIR/*
@@ -11,3 +11,4 @@ do
         diff -q "$FILE" "${TARGET}/${BASENAME}" 2>/dev/null && ln -vf "${TARGET}/${BASENAME}" "${FILE}"
     done
 done
+echo "Optimization completed"
